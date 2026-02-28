c383b3ca2bfda14920f6c1fda171a667911bb966
v4L0r <68015645+v4L0r@users.noreply.github.com>
2026-02-28 18:44:03 +0800

    Create User.js

---
+++ server/models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please add an email'],
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add a password'],
+    minlength: 6,
+    select: false,
+  },
+}, {
+  timestamps: true,
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.matchPassword = async function (plainPassword) {
+  return await bcrypt.compare(plainPassword, this.password);
+};
+
+<<<<<<< HEAD
+module.exports = mongoose.model('User', userSchema);
+=======
+module.exports = mongoose.model('User', userSchema);
+>>>>>>> origin/feature/auth-setup
